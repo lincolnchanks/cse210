71dc9988b847795f6d6bc9d19f14e47fd9feb77d
lincolnchanks <lchanks2024@gmail.com>
2025-10-30 09:10:04 -0600

    Print Hello World

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{ // void means: don't return anything. That's this function's return type. // Must have Main, must be capitalized.
+    static void Main(string[] args) 
     {
         Console.WriteLine("Hello Sandbox World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("Please enter your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Please enter your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName}.");
     }
 }
