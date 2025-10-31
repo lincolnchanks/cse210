f5b296ed9e9bc38198adc15ba5e0b0d5036d73a8
lincolnchanks <lchanks2024@gmail.com>
2025-10-30 21:10:03 -0600

    W01 Prove--Programming Part 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        int gradePercentage;
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentageAsText = Console.ReadLine();
+        gradePercentage = int.Parse(gradePercentageAsText);
+
+        string letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}!");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Better luck next time!");
+        }
     }
 }
