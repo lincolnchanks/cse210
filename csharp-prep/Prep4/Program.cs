845337091a0012cb39fb4d2a0d48cce8d0746536
lincolnchanks <lchanks2024@gmail.com>
2025-11-04 22:30:52 -0700

    Prep #4 - Sum, Average, Maximum

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int newNum;
+        do
+        {
+            Console.Write("Enter a number: ");
+            newNum = int.Parse(Console.ReadLine());
+            if (newNum != 0)
+            {
+                numbers.Add(newNum);
+            }
+        } while (newNum != 0);
+
+        int sum = 0;
+        double average;
+        int maximum = 0;
+
+        foreach (int n in numbers)
+        {
+            sum += n;
+            if (n > maximum)
+            {
+                maximum = n;
+            }
+        }
+        average = (double)sum / (double)numbers.Count;
+
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The largest number is {maximum}.");
+
+        // foreach (int n in numbers)
+        // {
+        //     Console.WriteLine($"{n}");
+        // }
     }
 }
