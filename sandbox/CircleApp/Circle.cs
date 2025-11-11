db41b573ca75f7e5ed3bffed6cb063cb526ab66a
lincolnchanks <lchanks2024@gmail.com>
2025-11-11 09:46:33 -0700

    Polished SetRadius method, added comments

--- sandbox/CircleApp/Circle.cs
+++ sandbox/CircleApp/Circle.cs
@@ -1,10 +1,21 @@
 class Circle
 {
     private double _radius; // Now that _radius is private, it's only usable inside this class.
-
+    // Keep **all of your data** private!
+    // If someone needs access to it, make access methods so they can access them ON YOUR TERMS.
     public void SetRadius(double radius)
     {
-        _radius = radius;
+        // SetRadius allows the user to set the radius, but now it must be set according to
+        // your standards. This keeps the data from being set to something invalid.
+        if (radius < 0)
+        {
+            Console.WriteLine("Invalid raidus, must be >= 0.0");
+            _radius = 0;
+        }
+        else
+        {
+            _radius = radius;
+        }
     }
 
     public double GetCircleArea()
