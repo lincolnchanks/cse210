1dc97a4bdad1af2b5dc8ebf786e19bdb3a3436f1
lincolnchanks <lchanks2024@gmail.com>
2025-11-24 09:26:13 -0700

    Using derived objects as their Parents

--- sandbox/PersonInheritance/Program.cs
+++ sandbox/PersonInheritance/Program.cs
@@ -1,5 +1,9 @@
 ﻿class Program
 {
+    public static void DisplayPersonInformation(Person person)
+    {
+        Console.WriteLine(person.PersonInformation());
+    }
     static void Main(string[] args)
     {
         Person johnny = new Person("Johnny", "West", 25, 165);
@@ -25,5 +29,21 @@
         
         Surgeon kaladinSurgeon = new Surgeon("Bridgeboy", "Shardspear", "Kaladin",
             "Stormblessed", 20, 200);
+        
+        Console.WriteLine("\n\n\n\n");
+        // Because of inheritance, you can add all of these child objects of Person to a list of Persons.
+        List<Person> myPeople = new List<Person>();
+        myPeople.Add(johnny);
+        myPeople.Add(waxilliumLadrian);
+        myPeople.Add(doctorLegundo);
+        myPeople.Add(kaladinStormblessed);
+        myPeople.Add(kaladinSurgeon);
+
+        // Because of inheritance you can also count these objects as instances of Person for any
+        // other code example.
+        foreach(Person person in myPeople)
+        {
+            DisplayPersonInformation(person);
+        }
     }
 }
