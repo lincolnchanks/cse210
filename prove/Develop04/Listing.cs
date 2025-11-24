0660680d42bf553d43dbf344dd5bd966c9cece78
lincolnchanks <lchanks2024@gmail.com>
2025-11-24 09:16:52 -0700

    Added Listing class

---
+++ prove/Develop04/Listing.cs
@@ -0,0 +1,13 @@
+class Listing : BaseActivity
+{
+    public Listing(string name, string description, int duration)
+    : base(name, description, duration)
+    {
+        
+    }
+
+    public void RunListingActivity()
+    {
+        
+    }
+}
