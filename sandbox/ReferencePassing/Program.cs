4ede0a76ce17d49cb042c23b433898195ebe4f7c
lincolnchanks <lchanks2024@gmail.com>
2025-11-25 09:31:20 -0700

    Reference vs. Value Types

--- sandbox/ReferencePassing/Program.cs
+++ sandbox/ReferencePassing/Program.cs
@@ -3,5 +3,16 @@
     static void Main(string[] args)
     {
         Console.WriteLine("I'm not happy Bob. Not happy.");
+
+        int x = 10;
+        int y = x;
+        y++;
+        Console.WriteLine($"{x} {y}");
+
+        int[] a = {1, 2, 3, 4, 5, 6};
+        int[] b = a;
+
+        b[3] = 111;
+        Console.WriteLine($"{a[3]}, {b[3]}");
     }
 }
