18aaa2ea73428889eb2e1e0bc75aed84abe602de
lincolnchanks <lchanks2024@gmail.com>
2025-11-25 09:15:37 -0700

    Worked on Listing class

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,5 +1,13 @@
 class Listing : BaseActivity
 {
+    private string[] _prompts = [
+        "Who are people you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    ];
+
     public Listing(string name, string description, int duration)
     : base(name, description, duration)
     {
@@ -9,8 +17,13 @@ class Listing : BaseActivity
     public void RunListingActivity()
     {
         Console.WriteLine(GetStartMessage());
+        Console.WriteLine(GetDescription());
 
-        
+        Random promptGen = new Random();
+        int promptIndex = promptGen.Next(5);
+        string currentPrompt = _prompts[promptIndex];
+        Console.WriteLine(currentPrompt);
+
 
         Console.WriteLine(GetEndMessage());
     }
