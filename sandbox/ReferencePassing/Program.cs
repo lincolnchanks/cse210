443ca8fcdfa66fd4e18f2c7153d39a95c04720c7
lincolnchanks <lchanks2024@gmail.com>
2025-11-25 09:37:10 -0700

    PassByReference() and PassByValue()

--- sandbox/ReferencePassing/Program.cs
+++ sandbox/ReferencePassing/Program.cs
@@ -1,5 +1,21 @@
 ﻿class Program
 {
+    static void PassByReference(ref int x)
+    {
+        // This changes the actual value of x in the rest of the code/memory.
+        // Instead of creating a copy of the variable x, this function references
+        // the original x and changes it.
+        x = 9999;
+        Console.WriteLine($"In the reference function, x is: {x}");
+    }
+    static void PassByValue(int x)
+    {
+        // When x is passed into the function another chunk of memory is created
+        // with x's value. Thereby this is a duplicate of x and when it is changed
+        // here, x-prime (the original x) is not changed.
+        x = 1001;
+        Console.WriteLine($"In the function, x is: {x}.");
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("I'm not happy Bob. Not happy.");
@@ -14,5 +30,11 @@
 
         b[3] = 111;
         Console.WriteLine($"{a[3]}, {b[3]}");
+
+        PassByValue(x);
+        Console.WriteLine($"In Main, x is: {x}.");
+
+        PassByReference(ref x);
+        Console.WriteLine($"In Main, x is: {x}.");
     }
 }
