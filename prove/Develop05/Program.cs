4f5924819712fa53e83701158dfaec84991763a9
lincolnchanks <lchanks2024@gmail.com>
2025-12-01 11:10:15 -0700

    Beginning Menu functions for Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         User lincolnHanks = new User();
+        Menu myMenu = new Menu();
+        int response = 0;
+        while (response != 6)
+        {
+            response = myMenu.GetUserAction();
+            switch (response)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }
     }
 }
