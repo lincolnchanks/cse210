89908794778dd006ff3a7c1959840097a535565a
lincolnchanks <lchanks2024@gmail.com>
2025-12-01 10:12:21 -0700

    Initial Commit: Goal class with attributes and constructor

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,15 @@
+abstract class Goal
+{
+    string _name;
+    string _description;
+    int _points;
+    bool _isDone;
+
+    public Goal(string name, string description, int numPoints)
+    {
+        _name = name;
+        _description = description;
+        _points = numPoints;
+        _isDone = false;
+    }
+}
