7f6de742ce2e93f2277a3a7dff14fe886a9fc016
lincolnchanks <lchanks2024@gmail.com>
2025-12-02 09:18:57 -0700

    Main Program

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,27 +2,29 @@ using System;
 
 class Program
 {
+    static string GetInputString(string inputMessage)
+    {
+        Console.WriteLine(inputMessage);
+        Console.Write("> ");
+        string returnString = Console.ReadLine();
+        return returnString;
+    }
     static void Main(string[] args)
     {
-        User lincolnHanks = new User();
-        Menu myMenu = new Menu();
-        int response = 0;
-        while (response != 6)
-        {
-            response = myMenu.GetUserAction();
-            switch (response)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-            }
-        }
+        Console.WriteLine("Hello World.");
     }
 }
+
+// string goalType = GetInputString("What type of goal are you creating? (simple, eternal, checklist)");
+                    // string goalName = GetInputString("Enter the name of the goal.");
+                    // string goalDescription = GetInputString("Enter a short description of the goal.");
+                    // int goalPoints = int.Parse(GetInputString("Enter the number of points for completion."));
+                    // if (goalType.ToLower() == "simple")
+                    // {
+                    //     SimpleGoal newSimpleGoal = new SimpleGoal(goalName, goalDescription, goalPoints);
+                    // }
+                    // else if (goalType.ToLower() == "checklist")
+                    // {
+                    //     int goalMaxTimes = int.Parse(GetInputString("Enter the max number of times to complete this goal."));
+                    //     int goalBonusPoints = int.Parse(GetInputString("Enter the bonus points for completing all of the goals."));
+                    // }
