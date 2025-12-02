b13885eec4e8b9b766bfe6a85e9d051d90fb1779
lincolnchanks <lchanks2024@gmail.com>
2025-12-02 09:28:26 -0700

    Added Switch Statement to Program.cs

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -17,6 +17,20 @@ class Program
         while (response != 6)
         {
             response = menu.DisplayMenu();
+
+            switch (response)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
         }
     }
 }
