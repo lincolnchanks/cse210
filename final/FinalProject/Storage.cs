a6040551e33e4eb9923eb6ba16a03803ab82137e
lincolnchanks <lchanks2024@gmail.com>
2025-12-05 17:45:04 -0700

    Shell: Storage.cs

---
+++ final/FinalProject/Storage.cs
@@ -0,0 +1,14 @@
+class Storage
+{
+    private string _name;
+    private List<FoodItem> _contents;
+
+    public Storage(string name)
+    {
+        _name = name;
+    }
+    public void AddItem(FoodItem item)
+    {
+        
+    }
+}
