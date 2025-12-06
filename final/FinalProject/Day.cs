08a731e4dbbf9cace83218b266a69d0be77da4e6
lincolnchanks <lchanks2024@gmail.com>
2025-12-05 17:51:04 -0700

    Shell: Day.cs

--- final/FinalProject/Day.cs
+++ final/FinalProject/Day.cs
@@ -2,4 +2,28 @@ class Day
 {
     private DateTime _date;
     private List<FoodItem> _expiringItems;
+    private Meal _breakfast;
+    private Meal _lunch;
+    private Meal _dinner;
+
+    public Day(int year, int month, int day)
+    {
+        _date = new DateTime(year, month, day);
+    }
+    public void SetBreakfast(Meal breakfast)
+    {
+        
+    }
+    public void SetLunch(Meal lunch)
+    {
+        
+    }
+    public void SetDinner(Meal dinner)
+    {
+        
+    }
+    public void AddItemExpiration(FoodItem expiringItem)
+    {
+        
+    }
 }
