788f958877a785b1baa5b4b34e50e9500c4c33bc
lincolnchanks <lchanks2024@gmail.com>
2025-12-10 09:17:18 -0700

    using static Months;

--- sandbox/Enumerations/Program.cs
+++ sandbox/Enumerations/Program.cs
@@ -1,4 +1,6 @@
-﻿enum Months {January, February, March, April, May, June, 
+﻿using static Months;
+
+enum Months {January, February, March, April, May, June, 
     July, August, September, October, November, December}
 
 class Program
@@ -8,9 +10,9 @@ class Program
         // int DECEMBER = 12;
         int[] DaysInTheMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        Months CurrentMonth = Months.December;
+        Months CurrentMonth = December;
 
-        Console.WriteLine($"The number of days in December is {DaysInTheMonth[(int)Months.December]}");
+        Console.WriteLine($"The number of days in December is {DaysInTheMonth[(int)December]}");
 
         Console.WriteLine($"The number of days in December is {DaysInTheMonth[(int)CurrentMonth]}");
     }
