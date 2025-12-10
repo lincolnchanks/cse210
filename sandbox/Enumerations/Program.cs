c6680eb835efd7759e612f197916eabe2c05357b
lincolnchanks <lchanks2024@gmail.com>
2025-12-10 09:16:47 -0700

    Enums Intro

---
+++ sandbox/Enumerations/Program.cs
@@ -0,0 +1,17 @@
+﻿enum Months {January, February, March, April, May, June, 
+    July, August, September, October, November, December}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // int DECEMBER = 12;
+        int[] DaysInTheMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        Months CurrentMonth = Months.December;
+
+        Console.WriteLine($"The number of days in December is {DaysInTheMonth[(int)Months.December]}");
+
+        Console.WriteLine($"The number of days in December is {DaysInTheMonth[(int)CurrentMonth]}");
+    }
+}
