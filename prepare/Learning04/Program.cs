03c2cefe7e3ce43b81c824348f0f02a1c3244f19
lincolnchanks <lchanks2024@gmail.com>
2025-12-15 20:05:39 -0700

    Learning04 Program.cs and Assignment class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Linguistics program!!! (Conlangs!!)
+        Assignment assignment1 = new Assignment("Bob Jones", "Phonology");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHashCode()); // Is this the memory location?
+        Console.WriteLine(assignment1.GetType()); // Gets the data type (Assignment)
     }
 }
