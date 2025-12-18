78e6a07c51cd319239692fa7a655f65d10d4a3eb
lincolnchanks <lchanks2024@gmail.com>
2025-12-17 23:04:49 -0700

    Getters and Setters for Fraction class

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -18,4 +18,20 @@ public class Fraction
         _top = top;
         _bottom = bottom;
     }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,5 +7,9 @@ class Program
         Fraction fraction1 = new Fraction();
         Fraction fraction2 = new Fraction(6);
         Fraction fraction3 = new Fraction(6/7);
+
+        Console.WriteLine($"{fraction2.GetTop()}/{fraction2.GetBottom()}");
+        fraction2.SetBottom(4);
+        Console.WriteLine($"{fraction2.GetTop()}/{fraction2.GetBottom()}");
     }
 }
